ba0364308bb5e1df20bee573c4905d95600e2450
Nestor <ncervan87@gmail.com>
2021-01-19 22:11:41 -0800

    changes main header from hello to hi

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
     return (
         <section className="homeContainer" id="home">
             <div className="homeIntro">
-                <h1>Hello!  I'm Nestor</h1>
+                <h1>Hi!  I'm Nestor.</h1>
                 <h3 className="introSubheader">A Full-Stack Web Developer</h3>
                 <button className="homeBtn">View Resume</button>
             </div>
