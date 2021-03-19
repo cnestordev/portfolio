3afa4317c36f609d67600f80809f90251e5f9121
Nestor <ncervan87@gmail.com>
2021-03-18 23:49:45 -0700

    changes skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -27,9 +27,9 @@ function Skills() {
     },
   ])
 
-  const skillArr = skillSet.map(skill => {
-    return <SkillBox skill={skill} />
-  })
+  // const skillArr = skillSet.map(skill => {
+  //   return <SkillBox skill={skill} />
+  // })
 
   return (
     <section
@@ -38,8 +38,46 @@ function Skills() {
       id="skills"
     >
       <Container>
-        <h3 className="skillsHeader subheader">Skills</h3>
-        <div className="skillSetContainer">{skillArr}</div>
+        <h3 className="skillsHeader subheader">
+          Tec <span className="techText">Skills</span>
+        </h3>
+        <div className="skills-flex">
+          <div className="skills-left side">
+            <img className="devImg" src="./dev.png" />
+          </div>
+          <div className="skills-right side">
+            <div className="frontend tech">
+              <h3>Frontend</h3>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>JavaScript</p>
+              <p>React</p>
+              <p>EJS</p>
+              <p>Bootstrap</p>
+              <p>Styled-Components</p>
+            </div>
+            <div className="backend tech">
+              <h3>Backend</h3>
+              <p>Node</p>
+              <p>Express</p>
+              <p>MongoDB</p>
+              <p>Mongoose</p>
+              <p>Postgres</p>
+              <p>PassportJS</p>
+              <p>REST</p>
+            </div>
+            <div className="other">
+              <h3>Other</h3>
+              <div className="misc">
+                <p>Git</p>
+                <p>NPM</p>
+                <p>Whimsical</p>
+                <p>Figma</p>
+                <p>Notion</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </Container>
     </section>
   )
